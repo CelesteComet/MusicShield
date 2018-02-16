139026ab28136446b2723b8cd252b00964f122de
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2018-02-16 08:06:25 -0800

    node

---
+++ server.js
@@ -0,0 +1,17 @@
+var port = process.env.PORT || 3000;
+var express = require('express')
+var app = express()
+var server = require('http').Server(app);
+var verbose = true;
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+})
+
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+})
